51ec526ba77bae588825d18f8d490c25b0430c7a
Patrick Maciel <patrickmaciel.ds@gmail.com>
2022-05-11 14:25:29 -0300

    Enviando atividade POO - Criacao de Menu

---
+++ FrmVerde.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploMenu
+{
+    public partial class FrmVerde : Form
+    {
+        public FrmVerde()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAmareloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAmarelo frm = new FrmAmarelo(); // indico qual form sera aberto
+            this.Hide(); // fechar o atual
+            frm.ShowDialog();
+        }
+
+        private void exibirFormAzul_Click(object sender, EventArgs e)
+        {
+            FrmAzul frm = new FrmAzul(); // indico qual form sera aberto
+            this.Hide(); // fechar o atual
+            frm.ShowDialog();
+        }
+
+        private void exibirFormVerde_Click(object sender, EventArgs e)
+        {
+            FrmVerde frm = new FrmVerde(); // indico qual form sera aberto
+            this.Hide(); // fechar o atual
+            frm.ShowDialog();
+        }
+    }
+}
